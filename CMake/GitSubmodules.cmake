99dcc09f8756ab0325d5315a631e58a9a99081dc
Edward Owen <eddw96@gmail.com>
2024-03-26 00:38:33 +0000

    Fixed git submodules issue with a fresh copy of the environment
    Also cleaned the format on CMakeLists files

--- CMake/GitSubmodules.cmake
+++ CMake/GitSubmodules.cmake
@@ -1,4 +1,4 @@
-function(add_git_submodule submodule_name)
+function(add_git_submodule submodule_path)
 # add a Git submodule directory to CMake, assuming the
 # Git submodule directory is a CMake project.
 #
@@ -7,10 +7,19 @@ function(add_git_submodule submodule_name)
 # include(AddGitSubmodule.cmake)
 # add_git_submodule(mysubmod_dir)
 
-if(NOT EXISTS ../external/${submodule_name}/CMakeLists.txt)
-  execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive -- ../external/${submodule_name}
-    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-    COMMAND_ERROR_IS_FATAL ANY)
-endif()
+  find_package(Git REQUIRED)
 
-endfunction(add_git_submodule)
+  # Check if the submodule directory exists
+  if(NOT EXISTS "${PROJECT_SOURCE_DIR}/${submodule_path}/.git")
+    zn_log("Initializing and updating submodule at ${submodule_path}")
+    # Initialize and update the submodule
+    execute_process(COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive ${submodule_path}
+                    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+                    RESULT_VARIABLE GIT_SUBMOD_RESULT
+                    OUTPUT_QUIET)
+    if(NOT GIT_SUBMOD_RESULT EQUAL "0")
+        zn_error(FATAL_ERROR "git submodule update --init failed with ${GIT_SUBMOD_RESULT}, please checkout submodules")
+    endif()
+  endif()
+
+endfunction()
